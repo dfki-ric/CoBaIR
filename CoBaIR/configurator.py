062003bc5da058d6d470986fa39c7659ea3a0cf0
Arunima Gopikrishnan <argo01@dfki.de>
2023-08-10 14:24:18 +0200

    Refactoring update config

--- CoBaIR/configurator.py
+++ CoBaIR/configurator.py
@@ -1246,7 +1246,6 @@ class Configurator(QtWidgets.QMainWindow):
                 f"QSlider::handle:horizontal {{background-color: {self.COLORS[value]}}}")
         except Exception as e:
             self.error_label.setText(str(e))
-        self.graph_item.update_value(context, intention)
         if context or intention is not None:
             self.graph_item.set_config(self.bayesNet.config)
         self.title_update()
